793c7c9dd4b6f58cb8ab00519d3dfe3a263012c5
joshua-segovia-intel <joshua.segovia@intel.com>
2024-04-19 16:32:23 -0400

    added a simple version of the langchain LLM - no gradio implementation on this version

--- recipes/ai-rag-amx-ubuntu/gradio_simple.py
+++ recipes/ai-rag-amx-ubuntu/gradio_simple.py
@@ -1,8 +1,42 @@
+import os
+import pandas as pd
+from langchain_community.llms import GPT4All
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.indexes import VectorstoreIndexCreator
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from datasets import load_dataset
+
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+model_path = "/data/models/ggml-model-gpt4all-falcon-q4_0.bin"
+n_threads=64
+max_tokens=100
+repeat_penalty=1.50
+n_batch=64
+top_k=2
+temp=0.7
 
-demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
-    
-demo.launch(share=True)  # Share your demo with just 1 extra parameter
+
+
+
+callbacks = [StreamingStdOutCallbackHandler()]
+
+llm = GPT4All(model=model_path, callbacks=callbacks, verbose=False,
+                    n_threads=n_threads, n_predict=max_tokens, repeat_penalty=repeat_penalty, 
+                    n_batch=n_batch, top_k=top_k, temp=temp)
+
+template = """Question: {question}
+            Answer: This is the response: """
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+
+question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+
+llm_chain.run(question)
