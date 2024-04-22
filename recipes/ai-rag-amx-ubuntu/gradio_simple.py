b744b37d02cafae08c6973bfd0eaba6287a3d0d8
chrisahsiong23 <chris.ah-siong@intel.com>
2024-04-22 14:16:06 -0700

    adding dropdown for using another model + adding capability to load a different model when chosen

--- recipes/ai-rag-amx-ubuntu/gradio_simple.py
+++ recipes/ai-rag-amx-ubuntu/gradio_simple.py
@@ -12,7 +12,11 @@ from datasets import load_dataset
 
 import gradio as gr
 
-model_path = "/data/models/ggml-model-gpt4all-falcon-q4_0.bin"
+# Defining paths to the different models 
+model_paths = {
+    "Model 1" : "/data/models/ggml-model-gpt4all-falcon-q4_0.bin",
+    "Model 2" : "/data/models/mistral_model"
+}
 n_threads=64
 max_tokens=100
 repeat_penalty=1.50
@@ -21,11 +25,11 @@ top_k=2
 temp=0.7
 
 
-
-
 callbacks = [StreamingStdOutCallbackHandler()]
 
-llm = GPT4All(model=model_path, callbacks=callbacks, verbose=False,
+#Setting and loading the default model
+selected_model_path = model_paths["Model 1"]
+llm = GPT4All(model=selected_model_path, callbacks=callbacks, verbose=False,
                     n_threads=n_threads, n_predict=max_tokens, repeat_penalty=repeat_penalty, 
                     n_batch=n_batch, top_k=top_k, temp=temp)
 
@@ -36,14 +40,30 @@ prompt = PromptTemplate(template=template, input_variables=["question"])
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
+
+def predict(question, selected_model):
+    # Load the selected model
+    selected_model_path = model_paths[selected_model]
+    llm = GPT4All(model=selected_model_path, callbacks=callbacks, verbose=False,
+                   n_threads=n_threads, n_predict=max_tokens, repeat_penalty=repeat_penalty,
+                   n_batch=n_batch, top_k=top_k, temp=temp)
+    llm_chain.llm = llm  # Update the llm in the LLMChain
+
+    # Get the answer using the updated model
+    answer = llm_chain.run(question)
+    return answer
+
+# Create a list of models for the dropdown
+model_choices = list(model_paths.keys())
+'''
 #question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
 def predict(question):
     answer = llm_chain.run(question)
     return answer
-
+'''
 iface = gr.Interface(
     fn=predict,
-    inputs="text",
+    inputs=["text", gr.inputs.Dropdown(model_choices, label="Select Model")],
     outputs="text",
     title="Question-Answer Chatbot",
     description="Enter your question below:",
