4b793faed514fbbacdc268b0b6cb27d9a21742fe
Steve Fowler <william.fowler@intel.com>
2024-07-25 14:59:17 -0500

    Update OPEA ChatQnA Example to use pre-built containers (#36)
    * Update recipe to use pre-built images
    
    * Update recipe
    
    * Update recipe
    
    Signed-off-by: Steve Fowler <william.fowler@intel.com>
    
    * Updated readme
    
    Signed-off-by: Steve Fowler <william.fowler@intel.com>
    
    ---------
    
    Signed-off-by: Steve Fowler <william.fowler@intel.com>

--- recipes/ai-opea-chatqna-xeon/opea.sh
+++ recipes/ai-opea-chatqna-xeon/opea.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 host_ip=$(ec2metadata --public-ipv4)
+export no_proxy=
 export http_proxy=
 export https_proxy=
 export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
@@ -8,6 +9,8 @@ export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
 export TEI_EMBEDDING_ENDPOINT="http://$host_ip:6006"
 export TEI_RERANKING_ENDPOINT="http://$host_ip:8808"
 export TGI_LLM_ENDPOINT="http://$host_ip:9009"
+export vLLM_LLM_ENDPOINT="http://${host_ip}:9009"
+export LLM_SERVICE_PORT=9000
 export REDIS_URL="redis://$host_ip:6379"
 export INDEX_NAME="rag-redis"
 export MEGA_SERVICE_HOST_IP=$host_ip
@@ -17,4 +20,8 @@ export RERANK_SERVICE_HOST_IP=$host_ip
 export LLM_SERVICE_HOST_IP=$host_ip
 export BACKEND_SERVICE_ENDPOINT="http://$host_ip:8888/v1/chatqna"
 export DATAPREP_SERVICE_ENDPOINT="http://$host_ip:6007/v1/dataprep"
+export DATAPREP_GET_FILE_ENDPOINT="http://$host_ip:6008/v1/dataprep/get_file"
+export DATAPREP_DELETE_FILE_ENDPOINT="http://$host_ip:6009/v1/dataprep/delete_file"
+export DOC_BASE_URL=''
 export host_ip=$host_ip
+#export HUGGINGFACEHUB_API_TOKEN="YourHuggingfaceToken"
