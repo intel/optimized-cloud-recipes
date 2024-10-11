b77fe8ac370bdb6b865248362702c728b0665876
Lucas Melo <lucas.melo@intel.com>
2024-10-11 16:24:57 -0500

    Update opea.sh

--- recipes/ai-opea-codegen-xeon/opea.sh
+++ recipes/ai-opea-codegen-xeon/opea.sh
@@ -3,8 +3,8 @@
 export no_proxy=
 export http_proxy=
 export https_proxy=
-export LLM_MODEL_ID="codellama/CodeLlama-7b-Instruct-hf"
-#export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
+#export LLM_MODEL_ID="codellama/CodeLlama-7b-Instruct-hf"
+export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
 export TGI_LLM_ENDPOINT="http://${host_ip}:8028"
 export MEGA_SERVICE_HOST_IP=${host_ip}
 export LLM_SERVICE_HOST_IP=${host_ip}
